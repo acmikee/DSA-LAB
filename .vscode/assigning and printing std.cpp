f565d4b420f4912f5de51d6e2b902a097a91ea20
acmikee <michaelashenafi97@gmail.com>
2025-03-22 23:00:46 +0300

    Delete .vscode directory

--- .vscode/assigning and printing std.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-			#include <string>  
-int main(){
-    std::string myString = "Initial string content";
-    std::cout << myString << std::endl;
-    myString = "Updated string content";
-    std::cout << myString << std::endl;
-    return 0;
-}
--- .vscode/helloworld.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(){
-    char letter = 'A'
-    int number = 100;
-    float decimalnumber = 3.14f;
-    bool istrue = true;
